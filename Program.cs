a81027ca09a6b5f5ff131899634014026fdc593f
ylvict <ylvict@gmail.com>
2014-12-31 01:00:51 -0800

    Add Project source

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Beep
+{
+    class BeepDemo
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                Beep(random.Next(10000), 100);
+            }
+        }
+    }
+}
